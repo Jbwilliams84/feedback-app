1c49660aacbba09825f918d3c015fe7734fa95cf
Jb Williams <jbwilliams484@gmail.com>
2022-03-15 15:11:06 -0500

    first comit

---
+++ src/data/FeedbackData.jsx
@@ -0,0 +1,20 @@
+export const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text:
+      "Hooble aslk ahsooq nasao aslaaaedwq asdlka  awlkdjaoiasj a.as.asdoiasd  awoidaj",
+  },
+  {
+    id: 2,
+    rating: 3,
+    text:
+      "Hooble aslk ahsooq nasao aslaaaedwq asdlka  awlkdjaoiasj a.as.asdoiasd  awoidaj",
+  },
+  {
+    id: 3,
+    rating: 5,
+    text:
+      "Hooble aslk ahsooq nasao aslaaaedwq asdlka  awlkdjaoiasj a.as.asdoiasd  awoidaj",
+  },
+];
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "../components/shared/Card";
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About this Project</h1>
+        <p>This is a react app to leave feedback for a product or service</p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
